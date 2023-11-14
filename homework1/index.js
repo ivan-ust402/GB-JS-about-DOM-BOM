1cfdd5624beeb78553dd096faa28fa8a63701fc7
Ivan Ustyantsev <vangold915@gmail.com>
2023-11-14 18:38:29 +0700

    Some bugs fixed

--- homework1/index.js
+++ homework1/index.js
@@ -3,23 +3,20 @@
 Отслеживание изменения ориентации экрана:
 Напишите код, который отслеживает изменение ориентации экрана устройства (с портретной на ландшафтную и наоборот) и выводит сообщение об этом на веб-странице.
 */
-console.log('------------------');
-console.log('Задание 1');
 
 window.addEventListener('DOMContentLoaded', () => {
     console.log(`Ориентация экрана: ${screen.orientation.angle === 90 ? 'ландшафная' : 'портретная'}`);
-})
+});
 
 window.addEventListener('orientationchange', () => {
     console.log(`Ориентация экрана: ${screen.orientation.angle === 90 ? 'ландшафная' : 'портретная'}`);
-})
+});
+
 /* 
 Задание 2
 Предупреждение о несохраненных данных:
 Создайте веб-форму с текстовым полем. Реализуйте функционал, при котором при попытке закрыть вкладку браузера появляется диалоговое окно с предупреждением о возможности потери введенных, но несохраненных данных.
 */
-console.log('------------------');
-console.log('Задание 2');
 
 const inputEl = document.querySelector('.input');
 
@@ -30,7 +27,7 @@ const inputEl = document.querySelector('.input');
         console.log(2)
         confirm('Закрытие окна приведет к потере несохраненных данных. Вы уверены, что хотите покинуть страницу, не отправив форму?');
     }
-}) */
+}); */
 
 const beforeUnloadHandler = event => {
     event.preventDefault();
@@ -39,7 +36,7 @@ const beforeUnloadHandler = event => {
 
     // Игнорируется по версии HTML спецификации
     // confirm('Закрытие окна приведет к потере несохраненных данных. Вы уверены, что хотите покинуть страницу, не отправив форму?');
-}
+};
 
 inputEl.addEventListener('input', event => {
     if(event.target.value.trim() !== '') {
@@ -47,4 +44,4 @@ inputEl.addEventListener('input', event => {
     } else {
         removeEventListener("beforeunload", beforeUnloadHandler, {capture: true});
     }
-})
+});
